57f6f265fc408f9b256028d0837a3ee7e62e03ed
Hejx <531601727@qq.com>
2016-10-25 13:44:13 +0800

    init

---
+++ lib/vue-list.vue
@@ -0,0 +1,140 @@
+<template lang="html">
+  <div class="vue-list" @scroll="handleScroll">
+    <ul>
+      <li class="line-top" :style="{height: lineTopHeight +'px'}"></li>
+      <li v-for="item in previewList">{{item.title}}</li>
+      <li class="line-bottom" :style="{height: lineBottomHeight +'px'}"></li>
+    </ul>
+    <div class="load-more-gif">loading...</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue-list',
+  props: {
+    list: {
+      type: Array,
+      required: true,
+      default: [],
+      twoWays: true
+    },
+    height: {
+      type: Number,
+      default: 44
+    },
+    canScroll: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+      lastScrollTop: null,
+      distance: 44,
+      lineTopHeight: 0,
+      lineBottomHeight: 0,
+      canLoadmore: true,
+      previewList: []
+    }
+  },
+  computed: {},
+  created() {},
+  mounted () {
+    this.initData();
+    this.handleScroll();
+  },
+  attached () {},
+  methods: {
+    initData() {
+      this._rowsInWindow = Math.ceil(this.$el.offsetHeight / this.height),
+      this._above = this._rowsInWindow * 2,
+      this._below = this._rowsInWindow,
+      this._max = this._rowsInWindow * this.height;
+    },
+    handleScroll() {
+      let _scrollTop = this.$el.scrollTop,
+          _height = this.$el.querySelectorAll('ul')[0].offsetHeight,
+          _contentHeight = this.$el.offsetHeight;
+
+      if (this.lastScrollTop === null || Math.abs(_scrollTop - this.lastScrollTop) > this._max) {
+          this.lastScrollTop = _scrollTop;
+      } else {
+          console.log(_height, _scrollTop, _contentHeight, _height-_scrollTop-_contentHeight);
+          if (this.to === this.list.length && _height - _scrollTop - _contentHeight < this.distance) {
+            this.canScroll && this.loadmore(this.from, this.to);
+            console.log('loadmore when go end');
+          }
+          return;
+      }
+
+      var _from = parseInt(_scrollTop / this.height) - this._above;
+      if (_from < 0) {
+          _from = 0;
+      }
+      var _to = _from + this._above + this._below + this._rowsInWindow;
+      if (_to > this.list.length) {
+          _to = this.list.length;
+      }
+
+      this.from = _from;
+      this.to = _to;
+      this.lineTopHeight = _from * this.height;
+      this.lineBottomHeight = (this.list.length - _to) * this.height;
+
+      this.previewList = [];
+      for (; _from < _to; _from++) {
+          this.previewList.push(this.list[_from]);
+      }
+    },
+    loadmore(from, to) {
+      if (!this.canLoadmore) return;
+      this.canLoadmore = false;
+      setTimeout(() => {
+        for(var i = 0; i < 200; i++) {
+          this.list.push({
+            title: 'list ' + new Date().getTime()
+          });
+        }
+        let _from = from, _to = to + this._below;
+        this.previewList = [];
+        for (; _from < _to; _from++) {
+            this.previewList.push(this.list[_from])
+        }
+        this.lineBottomHeight = (this.list.length - _to) * this.height;
+        this.handleScroll();
+
+        this.canLoadmore = true;
+      }, 2000)
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="less">
+.vue-list{
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  ul {
+    margin: 0;
+    padding: 0;
+    li{
+      text-decoration: none;
+      height: 44px;
+      font-size: 14px;
+      line-height: 3;
+      text-align: left;
+      padding-left: 20px;
+      border-bottom: 1px solid #ddd;
+    }
+  }
+  .load-more-gif{
+    width: 100%;
+    height: 44px;
+    text-align: center;
+    line-height: 44px;
+  }
+}
+</style>
---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <h1 class="title">vue-list</h1>
+    <div class="content">
+      <div class="preview">
+        <vue-list :list.sync='list'></vue-list>
+      </div>
+      <div class="setting">
+        setting
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import vueList from '../lib/vue-list';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    for(var i = 0; i < 200; i++) {
+      this.list.push({
+        title: 'list ' + new Date().getTime()
+      });
+    }
+  },
+  components: {
+    vueList
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  .title{
+    line-height: 50px;
+  }
+  .content{
+    display: flex;
+    width: 800px;
+    height: 500px;
+    margin: 0 auto;
+    &>div{
+      flex: 1;
+      height: 100%;
+    }
+  }
+}
+</style>
